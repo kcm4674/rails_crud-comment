44d8f0f7032fe8800ca35cef79257446109d90c4
ChulMin Kim <kbs4674@naver.com>
2019-06-30 05:48:37 +0000

    Step 2 : [Step 1에 이은 기본 설정] routes.rb에서 메인페이지에서 보여줄 view 설정

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'posts#index' # 주의 : controller(posts)와 view(index) 사이 구분은 #로 표기 (/로 하면 안됩니다!)
+  
   get 'posts/index'
   get 'posts/show'
   get 'posts/new'
