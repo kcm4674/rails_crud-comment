26c5dcaa6b37779a7861b160b28a15cde984a2bc
ChulMin Kim <kbs4674@naver.com>
2019-06-30 05:47:35 +0000

    Step 1 : posts controller/view 및 post model 생성

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
