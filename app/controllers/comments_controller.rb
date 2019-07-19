c5812107d6b05a9474790f630fd55f7b88d8d733
kbs4674 <kbs4674@naver.com>
2019-07-19 15:00:06 +0000

    Step 12 : Comment Controller 정의(설명 주석 있습니다.)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
+  
+  ## [METHOD : POST 방식 / post_comments_path] http://(홈페이지 루트주소)/posts/:post_id/comments
   def create
+    Comment.create(post_id: params[:post_id], body: params[:comment_body]) ## post_id는 주소에서 :post_id 를 통해 알아냅니다.
+    redirect_to request.referrer ## 이 전 페이지로 이동
   end
 
+  ## [METHOD : DELETE 방식 / post_comment_path] http://(홈페이지 루트주소)/posts/:post_id/comments/:id
   def destroy
+    @post = Post.find(params[:post_id]) ## post_id는 주소에서 :post_id 를 통해 알아냅니다.
+    @comment = @post.comments ## 해당 Post에 종속된 모든 Comment 데이터들을 불러옴.
+    @comment.find(params[:id]).destroy ## 특정 댓글의 ID값을 찾아내서 삭제
+    redirect_to request.referrer ## 이 전 페이지로 이동
   end
 end
